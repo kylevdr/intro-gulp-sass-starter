355ddf2cfb089a1b0b7ba6bd431760fe1c99d0f3
Kyle VanDeRiet <kylevdr@gmail.com>
2016-12-01 16:23:59 -0700

    Project complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,46 @@
-/*var gulp = require('gulp'),
-    del = require('del'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    print = require('gulp-print'),
-    babel = require('gulp-babel');
-    //babel-preset-es2015
-    */
-
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    print = require('gulp-print'),
+    babel = require('gulp-babel'),
+    del = require('del');
 
 var CacheBuster = require('gulp-cachebust');
 var cachebust = new CacheBuster();
 
+gulp.task('clean', function(cb) {
+    del(['dist'], cb);
+});
 
-// gulp.task('clean', function (cb) {
-//     del([
-//         'dist'
-//     ], cb);
-// });
+gulp.task('build-css', function() {
+    return gulp.src('./styles/*')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(cachebust.resources())
+        .pipe(concat('styles.css'))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist'));
+});
 
-// gulp.task('build-css', ['clean'], function () {
-//     return gulp.src('./styles/*')
-//         .pipe(sourcemaps.init())
-//         .pipe(sass())
-//         .pipe(cachebust.resources())
-//         .pipe(concat('styles.css'))
-//         .pipe(sourcemaps.write('./maps'))
-//         .pipe(gulp.dest('./dist'));
-// });
+gulp.task('build-js', function() {
+   return gulp.src('js/**/*.js')               
+      .pipe(sourcemaps.init())
+      .pipe(print())                        
+      .pipe(babel({ presets: ['es2015'] }))
+      .pipe(concat('bundle.js'))
+      .pipe(uglify())
+      .pipe(sourcemaps.write('./')) 
+      .pipe(gulp.dest('./dist/js')); 
+});
 
+gulp.task('build', ['build-css', 'build-js'], function() {
+    return gulp.src('index.html')
+        .pipe(cachebust.references())
+        .pipe(gulp.dest('dist'));
+});
 
-// gulp.task('build-js', ['clean'], function() {
-//    return gulp.src('js/**/*.js')               
-    //   .pipe(sourcemaps.init())
-    //   .pipe(print())                        
-    //   .pipe(babel({ presets: ['es2015'] }))
-    //   .pipe(concat('bundle.js'))
-    //   //.pipe(uglify())
-    //   .pipe(sourcemaps.write('./')) 
-    //   .pipe(gulp.dest('./dist/js')); 
-// });
-
-// gulp.task('build', [ 'clean', 'build-css', 'build-js'], function() {
-//     return gulp.src('index.html')
-//         .pipe(cachebust.references())
-//         .pipe(gulp.dest('dist'));
-// });
-
-// gulp.task('watch', function() {
-//     return gulp.watch(['./index.html','./partials/*.html', './styles/*.*css', './js/**/*.js'], ['build']);
-// });
+gulp.task('watch', function() {
+    return gulp.watch(['./index.html','./partials/*.html', './styles/*.*css', './js/**/*.js'], ['build']);
+});
